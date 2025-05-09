06c65922ac5e53af163135f28ce0466ec036a6dd
ding <dhq.s@qq.com>
2025-05-09 23:54:52 +0800

    22

--- scripts/config-repos.sh
+++ scripts/config-repos.sh
@@ -10,14 +10,15 @@ echo "检测到架构包: $ARCH_PACKAGES"
 # 禁用签名检查 
 sed -i 's/^option check_signature/# option check_signature/' repositories.conf  
  
-# 保留原有 kenzok8 源 
+# 添加源
+echo "src/gz kiddin9_packages https://dl.openwrt.ai/releases/24.10/packages/$ARCH_PACKAGES/kiddin9"  >> repositories.conf  
+
 echo "src/gz openwrt_kenzok8_package https://op.dllkids.xyz/packages/$ARCH_PACKAGES"  >> repositories.conf  
  
 # 添加 archive.openwrt.org  22.03 源 
 # echo "src/gz openwrt_archive_base https://archive.openwrt.org/releases/packages-22.03/$ARCH_PACKAGES/base"  >> repositories.conf  
 # echo "src/gz openwrt_archive_packages https://archive.openwrt.org/releases/packages-22.03/$ARCH_PACKAGES/packages"  >> repositories.conf  
 # echo "src/gz openwrt_archive_luci https://archive.openwrt.org/releases/packages-22.03/$ARCH_PACKAGES/luci"  >> repositories.conf  
-echo "src/gz  kiddin9_packages https://dl.openwrt.ai/releases/24.10/packages/$ARCH_PACKAGES/kiddin9"  >> repositories.conf  
  
 echo "当前软件源配置："
 cat repositories.conf  
