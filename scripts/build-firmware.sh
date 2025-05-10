c00485b488d20ce17e7e33702ccf6f77d5d9cfee
ding <dhq.s@qq.com>
2025-05-10 13:30:30 +0800

    5

--- scripts/build-firmware.sh
+++ scripts/build-firmware.sh
@@ -13,11 +13,58 @@ builder_dir=$(find . -maxdepth 1 -type d -name "immortalwrt-imagebuilder-*" | he
 [ -z "$builder_dir" ] && { echo "错误：未找到ImageBuilder目录"; exit 1; }
  
 # 直接复制files目录 
-echo "复制files目录到ImageBuilder..."
-cp -rf files "$builder_dir/" || { echo "错误：files目录复制失败"; exit 1; }
+# echo "复制files目录到ImageBuilder..."
+# cp -rf files "$builder_dir/" || { echo "错误：files目录复制失败"; exit 1; }
  
 # 进入编译目录 
-cd "$builder_dir" || exit 1 
+# cd "$builder_dir" || exit 1 
+
+mkdir -p files/etc/uci-defaults
+
+cat << "EOF" > files/etc/uci-defaults/99-custom
+# 使用 Argon 主题 
+uci set luci.main.mediaurlbase='/luci-static/argon' 
+
+# 禁用 IPv6 向导 
+uci set wizard.default.ipv6='0' 
+
+# Radio0 设置 (5GHz)
+uci set wireless.radio0.mu_beamformer='1' 
+uci set wireless.radio0.country='US' 
+uci set wireless.radio0.htmode='HE160' 
+uci set wireless.radio0.channel='149' 
+uci set wireless.default_radio0.ieee80211r='1'   # 802.11r 快速漫游
+ 
+# Radio1 设置 (2.4GHz)
+uci set wireless.radio1.mu_beamformer='1' 
+uci set wireless.radio1.country='US' 
+uci set wireless.default_radio1.ieee80211r='1'   # 802.11r 快速漫游
+
+# UPnP 服务启用
+uci set upnpd.config.enabled='1' 
+ 
+# 主机名设置
+uci set system.@system[0].hostname='QWRT'
+
+# 节点配置 (Socks5 代理)
+uci set passwall2.AVVjwzzU=nodes
+uci set passwall2.AVVjwzzU.remarks=' 自动切换'
+uci set passwall2.AVVjwzzU.type='Socks' 
+uci set passwall2.AVVjwzzU.address='127.0.0.1' 
+uci set passwall2.AVVjwzzU.port='1081' 
+ 
+# 全局代理设置
+uci set passwall2.@global[0].tcp_node='AVVjwzzU'
+ 
+# 本地 Socks5 服务端配置 
+uci set passwall2.oFIfqfES=socks
+uci set passwall2.oFIfqfES.enabled='1' 
+uci set passwall2.oFIfqfES.port='1081' 
+uci set passwall2.oFIfqfES.http_port='0' 
+uci set passwall2.oFIfqfES.enable_autoswitch='1' 
+
+uci commit
+EOF
 
 echo "当前工作目录: $(pwd)"
 echo "目录内容:"
