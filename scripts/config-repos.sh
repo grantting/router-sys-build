3643a5ca89559cfca0eb7167d62a64f9d3bd99af
ding <dhq.s@qq.com>
2025-08-08 23:21:03 +0800

    14

--- scripts/config-repos.sh
+++ scripts/config-repos.sh
@@ -23,26 +23,6 @@ echo "src/gz kiddin9_packages https://dl.openwrt.ai/releases/24.10/packages/$ARC
 echo "当前软件源配置："
 cat repositories.conf  
 
-echo '
-define KernelPackage/xdp-sockets-diag
-  SUBMENU:=$(NETWORK_SUPPORT_MENU)
-  TITLE:=PF_XDP sockets monitoring interface support for ss utility
-  KCONFIG:= \
-	CONFIG_XDP_SOCKETS=y \
-	CONFIG_XDP_SOCKETS_DIAG
-  FILES:=$(LINUX_DIR)/net/xdp/xsk_diag.ko
-  AUTOLOAD:=$(call AutoLoad,31,xsk_diag)
-endef
-
-define KernelPackage/xdp-sockets-diag/description
- Support for PF_XDP sockets monitoring interface used by the ss tool
-endef
-
-$(eval $(call KernelPackage,xdp-sockets-diag))
-' >> package/kernel/linux/modules/netsupport.mk
-
-echo 'CONFIG_PACKAGE_kmod-xdp-sockets-diag=y' >> .config
-
 # 解决默认设置冲突 
 echo "CONFIG_PACKAGE_default-settings-chn=y" >> .config 
 echo "CONFIG_PACKAGE_luci-lua-runtime=n" >> .config  # 显式禁用包
